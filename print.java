0e78452f17243ba7d9f8d3be210712b40f1f4b5f
sravanireddy1709-lang <sravani.reddy1709@gmail.com>
2025-12-26 20:45:23 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Print{
+public static void main(String[] args){
+int n;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+System.out.println("enter a num");
+for(int i=1;i<=n;i++)
+{
+System.out.println("sum"+i);
+}
+}
+}
